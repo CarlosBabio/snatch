974fc3af36292b5cf3cc1ddd21b45207100333a6
Mack Grissom <mcintyregrissom@gmail.com>
2022-05-31 15:29:46 +0800

    added new, create and private methods to bikes_controller

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,2 +1,19 @@
 class BikesController < ApplicationController
+    def new 
+        @bike = Bike.new
+    end
+
+    def create 
+        @bike = Bike.new(bike_params)
+    end
+
+    private 
+
+    def set_bike
+        @bike = Bike.find(params[:id])
+    end
+
+    def bike_params
+        params.require(:bike).permit(:title, :description, :address, :category, :engine_size, :license_plate, :price_per_day)
+    end
 end
