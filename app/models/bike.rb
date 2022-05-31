0927c9613a32418719e43c7d8bcbf42858f3aca2
Paul Hattabe <hattabe.p@gmail.com>
2022-05-31 15:00:29 +0800

    fix: added maximum to bikes title and description validation

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -6,8 +6,8 @@ class Bike < ApplicationRecord
   has_many :bookmarks
   has_many :users, through: :bookings
 
-  validates :title, presence: true, length: { minimum: 15 }
-  validates :description, presence: true, length: { minimum: 50 }
+  validates :title, presence: true, length: { minimum: 15, maximum: 50 }
+  validates :description, presence: true, length: { minimum: 50, maximum: 200 }
   validates :address, presence: true
   validates :category, presence: true, inclusion: { in: CATEGORIES }
   validates :engine_size, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 50 }
