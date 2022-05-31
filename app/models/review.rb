5dbd311fdf0d000385c74ffc180467e44f0ba995
Carlos Babio <carlos.babio@live.com.au>
2022-05-31 16:51:01 +0800

    Fixed small bug in Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :booking
   validates :rating, :comment, presence: true
-  validates :comment, length: {minimum: 5}, {maximum: 150}
-  validates :rating, length: {minimum: 1}, {maximum: 5}
+  validates :comment, length: { minimum: 5, maximum: 150 }
+  validates :rating, length: { minimum: 1, maximum: 5 }
 end
