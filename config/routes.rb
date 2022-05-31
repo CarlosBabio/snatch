e663ea652cbb9b901eb5faa65e98dfb58ac51b52
Paul Hattabe <hattabe.p@gmail.com>
2022-05-31 14:55:21 +0800

    added all routes for bikes, user, bookmarks, bookings, reviews models

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :bikes do
+    resources :bookmarks, only: %w[new create]
+    resources :bookings, only: %w[new create]
+  end
+
+  resources :bookmars, only: %w[index destroy]
+
+  resources :bookings, only: %w[show edit update] do
+    resources :reviews, only: %w[new create]
+  end
+
+  resources :users, only: %w[show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
