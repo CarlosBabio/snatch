97331ca37812294537889584479e1a15cf9bde5c
Mack Grissom <mcintyregrissom@gmail.com>
2022-05-31 14:37:06 +0800

    validations on booking and review models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :bike
   belongs_to :user
+  validates :start_date, :end_date, validates_date: true
+  validates :total_price, presence: true, numericality: {only_float: true}
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :booking
+  validates :rating, :comment, presence: true
+  validates :rating, length: {minimum: 1}, {maximum: 5}
 end
