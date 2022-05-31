549cdd26f43c4f002de500af72ff4a2067522755
Carlos Babio <carlos.babio@live.com.au>
2022-05-31 18:39:25 +0800

    Added review to bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :bike
   belongs_to :user
+  has_one :review
   validates :total_price, presence: true, numericality: {only_float: true}
   validates :start_date, presence: true
   validates :end_date, presence: true
