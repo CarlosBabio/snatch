31f01874ecf4f1e3c8d1b573361535ac741bb2cd
Carlos Babio <carlos.babio@live.com.au>
2022-05-31 16:06:29 +0800

    Cleaned up bike controll page

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,32 +1,41 @@
 class BikesController < ApplicationController
-  before_action :set_bike, only: [:edit, :update, :show, :destroy]
-    def show
-      @bike = Bike.find(params[:id])
-    end
+  before_action :set_bike, only: %i[edit update show destroy]
+  def show
+    @bike = Bike.find(params[:id])
+  end
 
-    def new 
-        @bike = Bike.new
-    end
+  def new
+    @bike = Bike.new
+  end
 
-    def create 
-        @bike = Bike.new(bike_params)
-    end
-  
-    def edit
-    end
+  def create
+    @bike = Bike.new(bike_params)
+  end
 
-    def update
-      @bike.update(bike_params)
-      redirect_to  bike_path(@bike)
-    end
+  def edit
+  end
 
-    private 
+  def update
+    @bike.update(bike_params)
+    redirect_to  bike_path(@bike)
+  end
 
-    def set_bike
-        @bike = Bike.find(params[:id])
-    end
+  private
 
-    def bike_params
-        params.require(:bike).permit(:title, :description, :address, :category, :engine_size, :license_plate, :price_per_day, :additional_info)
-    end
+  def set_bike
+    @bike = Bike.find(params[:id])
+  end
+
+  def bike_params
+    params.require(:bike).permit(
+      :title,
+      :description,
+      :address,
+      :category,
+      :engine_size,
+      :license_plate,
+      :price_per_day,
+      :additional_info
+    )
+  end
 end
