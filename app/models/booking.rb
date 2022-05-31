1d2eabcc4f4596a01454754f002a0989e82c9182
Mack Grissom <mcintyregrissom@gmail.com>
2022-05-31 14:48:05 +0800

    updating validations on review and booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :bike
   belongs_to :user
   validates :total_price, presence: true, numericality: {only_float: true}
+  validates :start_date, presence: true
+  validates :end_date, presence:
 end
