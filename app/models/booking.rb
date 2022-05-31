59d93e3032a6673c0ac2e23c0b437907f6563060
Mack Grissom <mcintyregrissom@gmail.com>
2022-05-31 14:55:11 +0800

    small fix on end_date validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :user
   validates :total_price, presence: true, numericality: {only_float: true}
   validates :start_date, presence: true
-  validates :end_date, presence:
+  validates :end_date, presence: true
 end
