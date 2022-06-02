cd5d65db53aaffc202ef0844b2cbfb5b7ba151b1
Carlos Babio <carlos.babio@live.com.au>
2022-06-02 14:25:53 +0800

    added create bookings feature

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,4 +5,12 @@ class Booking < ApplicationRecord
   validates :total_price, presence: true, numericality: { only_float: true }
   validates :start_date, presence: true
   validates :end_date, presence: true
+
+  before_validation :calculate_total_price
+
+  def calculate_total_price
+    return unless start_date && end_date
+
+    self.total_price = (bike.price_per_day * (end_date - start_date).to_i).to_i
+  end
 end
